d707225935caea17d6d7106a940a0b7614a09ad0
kodekill <brian.falor@gmail.com>
2017-04-10 22:32:08 -0600

    Update launcher.sh
    removed some code that shouldn't be there

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#launcher.sh
+
 sleep 15 
 cd /
 
@@ -7,11 +7,4 @@ cd /
 cd home/pi/Address
 /sbin/ifconfig > addr.txt
 sudo python address.py
-cd /
-
-
-
-#These lines will turn the camera on and start recording on boot
-#cd home/pi/LOAVES
-#sudo python Camera_Project.py
 cd /
