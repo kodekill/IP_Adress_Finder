a263eb42d4d3f59640e9b58670efa2dba39cbcd7
kodekill <15446993+kodekill@users.noreply.github.com>
2019-03-20 22:36:11 -0600

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-sleep 15 
+sleep 15
 cd /
 
+cd home/pi/Public/IP_Address_Finder
 
-cd home/pi/Address
-/sbin/ifconfig > addr.txt
+/sbin/ifconfig | grep -A 3 wlan0 | grep -m 1 inet > addr.txt
 sudo python address.py
+
 cd /
